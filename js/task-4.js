dff6f67abdf8a25dba67fdde9b126ee4037aa22c
NataliiaShah <gavrilovskaboroluk@gmail.com>
2024-09-02 22:33:41 +0300

    Update task-4.js
    correction

--- js/task-4.js
+++ js/task-4.js
@@ -1,23 +1,32 @@
-function getShippingCost(country)
+function getShippingCost(country) {
+  let message;
 
-{
-    let price;
-    switch (country) {
-        case "China":
-            return `Shipping to ${country} will cost 100 credits`;
-            break;
-         case "Chile":
-            return `Shipping to ${country} will cost 250 credits`;
-            break;
-         case "Australia":
-            return `Shipping to ${country} will cost 170 credits`;
-            break;
-         case "Jamaica":
-            return `Shipping to ${country} will cost 120 credits`;
-            break;
-        default:
-           return "Sorry, there is no delivery to your country";
-    }
+switch (country) {
+  case "China":
+    price = 100;
+    message = `Shipping to ${country} will cost ${price} credits`;
+    break;
+    
+    case "Chile":
+   price = 250;
+    message = `Shipping to ${country} will cost ${price} credits`;
+    break;
+
+    case "Australia":
+    price = 170;
+    message = `Shipping to ${country} will cost ${price} credits`;
+    break;
+
+    case "Jamaica":
+    price = 120;
+    message = `Shipping to ${country} will cost ${price} credits`;
+    break;
+
+  default:
+    message = "Sorry, there is no delivery to your country";
+}
+
+  return message;
 }
 
 
